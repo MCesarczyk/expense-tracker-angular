570f52164c938d413a789ad94750d0f525c19706
Michal Cesarczyk <michal.cesarczyk@gmail.com>
2024-09-21 23:36:07 +0200

    feat: create main app view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    imports: [RouterOutlet],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.less'
 })
 export class AppComponent {
-  title = 'expense-tracker';
+    title = 'expense-tracker';
 }
